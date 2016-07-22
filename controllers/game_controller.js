2af5e30cdd6cc06a1715194826c5f2edf00d3e79
Kirby Pagunsan <kirbypagunsan@Kirbys-MacBook-Pro.local>
2016-07-22 16:03:55 -0400

    ORM calls with predetermined variables in server.js work

--- controllers/game_controller.js
+++ controllers/game_controller.js
@@ -1,69 +1,73 @@
-//requires files to process requests to server
-var express = require('express');
-var bodyParser = require('body-parser');
-var orm = require('../config/orm.js');
-var app = express();
+// $(document).ready(function () {
 
-var winArray = ["Scotch","Key","Phone"];
+	//requires files to process requests to server
+	var express = require('express');
+	var bodyParser = require('body-parser');
+	var orm = require('../config/orm.js');
+	var app = express();
 
-var otherArray = ["Magnifier","Rope","Candlestick"];
-//game play data 
-var gameData = [
-	{name: "Bedroom", roomClass: "bedroom", frameClass: "frameIMG", text: 'you see me', button1: winArray[1], button2: otherArray[1]},
-	{name: "Library", roomClass: "library", frameClass: "frameIMG", text: 'you see me', button1: winArray[2], button2: otherArray[2]},
-	{name: "Maid's Room", roomClass: "maidsRoom", frameClass: "frameIMG", text: 'you see me', button1: otherArray[0], button2: winArray[0]},
-];
+	var winArray = ["Scotch","Key","Phone"];
 
-var game = require('../models/game.js');
+	var otherArray = ["Magnifier","Rope","Candlestick"];
+	//game play data 
+	var gameData = [
+		{name: "Bedroom", roomClass: "bedroom", frameClass: "frameIMG", text: 'you see me', button1: winArray[1], button2: otherArray[1]},
+		{name: "Library", roomClass: "library", frameClass: "frameIMG", text: 'you see me', button1: winArray[2], button2: otherArray[2]},
+		{name: "Maid's Room", roomClass: "maidsRoom", frameClass: "frameIMG", text: 'you see me', button1: otherArray[0], button2: winArray[0]},
+	];
 
-//page render program
-module.exports = function(app){
-	app.get('/end', function(req, res) {
-			res.render('end');
-		});
-	//logic to cover game beginning to end
-	if(gameData.length > 0) {
-		//home page / sign-in form 
-		app.get('/', function(req, res) {
-			res.render('home');
-		});
-		//game pages with random starting position
-		app.get('/game', function(req, res) {
-			var randomIndex = Math.floor(Math.random()*gameData.length);
-			var chosen = gameData[randomIndex];
-			console.log(chosen);
-			res.render('game', chosen);
-			gameData.splice(randomIndex, 1);
-		});
-	} else {
-		//final page to display results and scores
+	var game = require('../models/game.js');
+
+	//page render program
+	module.exports = function(app){
 		app.get('/end', function(req, res) {
-			res.render('end');
-		});
-	}
-}
+				res.render('end');
+			});
+		//logic to cover game beginning to end
+		if(gameData.length > 0) {
+			//home page / sign-in form 
+			app.get('/', function(req, res) {
+				res.render('home');
+			});
+			//game pages with random starting position
+			app.get('/game', function(req, res) {
+				var randomIndex = Math.floor(Math.random()*gameData.length);
+				var chosen = gameData[randomIndex];
+				console.log(chosen);
+				res.render('game', chosen);
+				gameData.splice(randomIndex, 1);
+			});
+		} else {
+			//final page to display results and scores
+			app.get('/end', function(req, res) {
+				res.render('end');
+			});
+		}
+	};
 
-// **************************************
-$(".btn").click(function(){
+	// **************************************
+	// $(".btn").click(function(){
 
-    name=$("#playerName").val();
-    pass=$("#playerPass").val();
-    
-    console.log(name + pass);
-    // * Perform some validation here.
-    
-	var playerName = $("input[id='playerName']").val();
-	var playerPass = $("input[id='playerPass']").val();
+	//     name=$("#playerName").val();
+	//     pass=$("#playerPass").val();
+	    
+	//     console.log(name + pass);
+	//     // * Perform some validation here.
+	    
+	// 	var playerName = $("input[id='playerName']").val();
+	// 	var playerPass = $("input[id='playerPass']").val();
 
-	console.log(playerName + playerPass);
-	// orm.checkSavedPlayer('game_data', 'player_name', playerName, playerPass);
-	game.newPlayer(playerName, playerPass);
+	// 	console.log(playerName + playerPass);
+	// 	// orm.checkSavedPlayer('game_data', 'player_name', playerName, playerPass);
+	// 	game.newPlayer(playerName, playerPass);
 
-   //  $.post("http://localhost:3000/login",{name:name,pass:pass},function(data){        
-   //      if(data==='done')           
-   //      {
-  	// console.log(name);
-   //        window.location.href="/admin";
-   //      }
-   //  });
-});
+	   //  $.post("http://localhost:3000/login",{name:name,pass:pass},function(data){        
+	   //      if(data==='done')           
+	   //      {
+	  	// console.log(name);
+	   //        window.location.href="/admin";
+	   //      }
+	   //  });
+	// });
+// });
+
--- server.js
+++ server.js
@@ -40,3 +40,22 @@ app.listen(port, function() {
     console.log("Listening on PORT " + port);
 });
 
+
+// ******************************************************
+var game = {
+    updateItem: function(item, bool, name) {
+    	orm.updateItem('game_data', item, bool, 'player_name', name);
+    },
+    checkSavedPlayer: function(name, password){
+    	orm.checkSavedPlayer('game_data', 'player_name', name, password);
+    },
+    newPlayer: function(name, password) {
+    	orm.newPlayer('game_data', name, password);
+    },    
+    // Checks if password input matches password column associated with username
+    authPassword: function(name, password) {
+    	orm.authPassword('game_data', 'player_name', name, password);
+    }
+};
+
+game.newPlayer("Woody", "Andy");
