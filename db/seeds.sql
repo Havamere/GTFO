056ee3c7c32cdcf845dfd3fcd7a617851e262043
Kyle R <kyle.rinehart83@gmail.com>
2016-07-23 12:49:28 -0400

    fixed jaws db mysql errors

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,17 +2,17 @@
  
  
 ### These players have finished the game and their score has been recorded and inserted into the database table
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Dark Helmet', '12345', true, false, true, false, true, false, 1, 60, 60);
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Leroy Jenkins', 'WOWislife', true, false, true, false, true, false, 5, 270, 1350);
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Dan Druff', 'headandshoulders', true, false, true, false, true, false, 4, 125, 500);
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Jim Imma', 'pancakes', true, false, true, false, true, false, 2, 240, 480);
 
 ### These players have not yet finished the game
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Mr Meeseeks', 'lookatme', false, true, false, true, false, true, 1, 75, null);
-INSERT INTO game_data (player_name, password, scotch_win, magnifier_lose, key_win, rope_lose, phone_win, candlestick_lose, tries, finish_time, score) 
+INSERT INTO game_data (player_name, password, Scotch, Magnifier, `Key`, Rope, Phone, Candlestick, tries, finish_time, score) 
 	VALUES ('Seymour Butz', 'lolz', false, true, true, false, true, false, 4, 210, null);
