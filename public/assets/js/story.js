b33b26ee82256d7cb1f589f4d75c01f6d4eecbf0
Kyle R <kyle.rinehart83@gmail.com>
2016-07-23 01:01:24 -0400

    playing with stories capabilities

---
+++ public/assets/js/story.js
@@ -0,0 +1,11 @@
+var stories = {
+	library: "I say, this whole house may be a ruse! No bother, I will take a look around. This is the largest library I have ever seen! Just look at all these books! I have some time to waste let me relax and read before moving on. I just want to have a quick peek around... ",
+	maidBed: "My word, this room is filthy! This must be the maid’s bedroom from the look of the small bed and general size of the room. It is very charming, although it may need some dusting and it is a bit drafty. I do not like the look of that pit below outside the window. I wonder what's in here... ",
+	masterBed: "I wonder who the master of this bedroom is. It is very large but very cozy, especially with that fire burning. A bit messy with the children’s toys around but nothing a little sprucing up won’t help with! Let me have a look around and see what I can find...",
+	attic: "Wow! Look at that window and how much space is in here! All that’s taking up space is this charming little children’s rocking horse and an antique dresser. They should move all that junk from downstairs to up here. They could really get this house to function better. Nevertheless, I must try to get out of here with what I have collected.",
+	useful: "These looks like they might be useful...",
+	win: "I'll just use the phone to call a local repair man to fix my buggy.  This key should unlock the door get out of this place.  As for the Scotch... Waste not, Want not.  Time for a nice drink.",
+	lose: "What am I supposed to do with these items?  I can't make heads or tails out of a solution to my predicament.  I'll need to try again."
+}
+
+module.exports = stories;
